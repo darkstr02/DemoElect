1a56c611cdaeb8ec2e21374cc35f4b5faa705ad0
Francisco Rodriguez <darkstr.300592@gmail.com>
2016-08-17 16:30:21 -0600

    segundo commit

--- EleccionesDemo/app/src/main/java/com/elecciones/francisco/eleccionesdemo/PuntosCasillas.java
+++ EleccionesDemo/app/src/main/java/com/elecciones/francisco/eleccionesdemo/PuntosCasillas.java
@@ -4,6 +4,33 @@ package com.elecciones.francisco.eleccionesdemo;
  * Created by Francisco on 17/08/2016.
  */
 public class PuntosCasillas {
+    private String id;
+    private String nombre_casilla;
+    private String domicilio;
+    private int lat;
+    private int longi;
 
-    
+    public String getId() {
+        return this.id;
+    }
+
+    public String getNombre_casilla()
+    {
+        return this.nombre_casilla;
+    }
+
+    public String getDomicilio()
+    {
+        return this.domicilio;
+    }
+
+    public int getLat()
+    {
+        return this.lat;
+    }
+
+    public int getLongi()
+    {
+        return this.longi;
+    }
 }
